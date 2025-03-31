62784717ab0e87f376502cfc38dab2bdc80f33ef
Jialu Pi <165834545+OrchidPi@users.noreply.github.com>
2025-03-31 00:22:45 -0700

    Update train.sh

--- MOSCARD/scripts/train.sh
+++ MOSCARD/scripts/train.sh
@@ -13,45 +13,45 @@ case $choice in
     1)
         echo "Running Baseline..."
         PYTHONPATH=.:$PYTHONPATH python MOSCARD/bin/train.py \
-        MOSCARD/config/Mayo.json \
-        MCAT_vit_losssave \
+        MOSCARD/config/config.json \
+        Multimodal_baseline \
         --num_workers 8 --device_ids "0" \
         --pre_train_backbone \
-        /media/Datacenter_storage/jialu/ECG/logdir/logdir-pretrained_vit/best2.ckpt \
-        /media/Datacenter_storage/jialu_/jialu_causalv2/logdir/logdir_pretrain_vit_new/best3.ckpt \
+        ECG/ckpt/ECG.ckpt \
+        CXR/ckpt/CXR.ckpt \
         --model "main" --logtofile True
         ;;
     2)
         echo "Running Causal..."
         PYTHONPATH=.:$PYTHONPATH python MOSCARD/bin/train.py \
-        /media/Datacenter_storage/jialu/003/MACE/Multimodal_MACE/config/Mayo.json \
-        MCAT_vit_losssave_causal \
-        --num_workers 8 --device_ids "3" \
+        MOSCARD/config/config.json \
+        Multimodal_causal \
+        --num_workers 8 --device_ids "0" \
         --pre_train_backbone \
-        /media/Datacenter_storage/jialu/ECG/logdir/logdir-pretrained_vit/best2.ckpt \
-        /media/Datacenter_storage/jialu_/jialu_causalv2/logdir/logdir_pretrain_vit_new/best3.ckpt \
+        ECG/ckpt/ECG.ckpt \
+        CXR/ckpt/CXR.ckpt \
         --model "causal" --logtofile True
         ;;
     3)
         echo "Running Conf..."
         PYTHONPATH=.:$PYTHONPATH python MOSCARD/bin/train.py \
-        /media/Datacenter_storage/jialu/003/MACE/Multimodal_MACE/config/Mayo.json \
-        MCAT_vit_losssave_conf \
-        --num_workers 8 --device_ids "3" \
+        MOSCARD/config/config.json \
+        Multimodal_conf \
+        --num_workers 8 --device_ids "0" \
         --pre_train_backbone \
-        /media/Datacenter_storage/jialu/003/logdir/logdir-pretrain_vit_conf_ecg/best2.ckpt \
-        /media/Datacenter_storage/jialu/003/logdir/logdir_pretrain_vit_conf_cxr/best3.ckpt \
+        ECG/ckpt/ECG_conf.ckpt \
+        CXR/ckpt/CXR_conf.ckpt \
         --model "main" --logtofile True
         ;;
     4)
         echo "Running CaConf..."
         PYTHONPATH=.:$PYTHONPATH python MOSCARD/bin/train.py \
-        /media/Datacenter_storage/jialu/003/MACE/Multimodal_MACE/config/Mayo.json \
-        /MCAT_vit_losssave_caconf \
-        --num_workers 8 --device_ids "3" \
+        MOSCARD/config/config.json \
+        Multimodal_caconf \
+        --num_workers 8 --device_ids "0" \
         --pre_train_backbone \
-        /media/Datacenter_storage/jialu/003/logdir/logdir-pretrain_vit_conf_ecg/best2.ckpt \
-        /media/Datacenter_storage/jialu/003/logdir/logdir_pretrain_vit_conf_cxr/best3.ckpt \
+        ECG/ckpt/ECG_conf.ckpt \
+        CXR/ckpt/CXR_conf.ckpt \
         --model "causal" --logtofile True
     
         echo "Invalid choice."
