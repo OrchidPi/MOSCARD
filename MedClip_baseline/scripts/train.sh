eac7b86e017cf722461b2b1954818fea417b0243
Jialu Pi <165834545+OrchidPi@users.noreply.github.com>
2025-03-30 23:33:36 -0700

    Update train.sh

--- MedClip_baseline/scripts/train.sh
+++ MedClip_baseline/scripts/train.sh
@@ -1,59 +1,29 @@
 #!/bin/bash
 
 echo "Select which mode to run:"
-echo "1) Baseline"
-echo "2) Causal"
-echo "3) Conf"
-echo "4) CaConf"
+echo "1) Alignment"
+echo "2) Classifier"
 
 
-read -p "Enter your choice [1-4]: " choice
+read -p "Enter your choice [1-2]: " choice
 
 case $choice in
     1)
-        echo "Running Baseline..."
-        PYTHONPATH=.:$PYTHONPATH python MOSCARD/bin/train.py \
-        MOSCARD/config/Mayo.json \
-        MCAT_vit_losssave \
-        --num_workers 8 --device_ids "0" \
-        --pre_train_backbone \
-        /media/Datacenter_storage/jialu/ECG/logdir/logdir-pretrained_vit/best2.ckpt \
-        /media/Datacenter_storage/jialu_/jialu_causalv2/logdir/logdir_pretrain_vit_new/best3.ckpt \
-        --model "main" --logtofile True
+        echo "Running MedClip Baseline Classify..."
+        PYTHONPATH=.:$PYTHONPATH python MedClip_baseline/bin/trian.py 
+        MedClip_baseline/config/config.json MedClip_Classify 
+        --num_workers 8 --device_ids "0,1" 
+        --pre_train MedClip_baseline/ckpt/alignment.ckpt
+        --model "main" --model_step "Classifier"--logtofile True
         ;;
     2)
-        echo "Running Causal..."
-        PYTHONPATH=.:$PYTHONPATH python MOSCARD/bin/train.py \
-        /media/Datacenter_storage/jialu/003/MACE/Multimodal_MACE/config/Mayo.json \
-        MCAT_vit_losssave_causal \
-        --num_workers 8 --device_ids "3" \
-        --pre_train_backbone \
-        /media/Datacenter_storage/jialu/ECG/logdir/logdir-pretrained_vit/best2.ckpt \
-        /media/Datacenter_storage/jialu_/jialu_causalv2/logdir/logdir_pretrain_vit_new/best3.ckpt \
-        --model "causal" --logtofile True
-        ;;
-    3)
-        echo "Running Conf..."
-        PYTHONPATH=.:$PYTHONPATH python MOSCARD/bin/train.py \
-        /media/Datacenter_storage/jialu/003/MACE/Multimodal_MACE/config/Mayo.json \
-        MCAT_vit_losssave_conf \
-        --num_workers 8 --device_ids "3" \
-        --pre_train_backbone \
-        /media/Datacenter_storage/jialu/003/logdir/logdir-pretrain_vit_conf_ecg/best2.ckpt \
-        /media/Datacenter_storage/jialu/003/logdir/logdir_pretrain_vit_conf_cxr/best3.ckpt \
-        --model "main" --logtofile True
-        ;;
-    4)
-        echo "Running CaConf..."
-        PYTHONPATH=.:$PYTHONPATH python MOSCARD/bin/train.py \
-        /media/Datacenter_storage/jialu/003/MACE/Multimodal_MACE/config/Mayo.json \
-        /MCAT_vit_losssave_caconf \
-        --num_workers 8 --device_ids "3" \
-        --pre_train_backbone \
-        /media/Datacenter_storage/jialu/003/logdir/logdir-pretrain_vit_conf_ecg/best2.ckpt \
-        /media/Datacenter_storage/jialu/003/logdir/logdir_pretrain_vit_conf_cxr/best3.ckpt \
-        --model "causal" --logtofile True
-    
+        echo "Running MedClip Baseline Classify..."
+        PYTHONPATH=.:$PYTHONPATH python MedClip_baseline/bin/trian.py 
+        MedClip_baseline/config/config.json MedClip_alignment 
+        --num_workers 8 --device_ids "0,1" 
+        --pre_train_backbone MedClip_baseline/ckpt/CXR.ckpt MedClip_baseline/ckpt/ECG.ckpt 
+        --model "main" --model_step "Classifier"--logtofile True
+
         echo "Invalid choice."
         ;;
 esac
